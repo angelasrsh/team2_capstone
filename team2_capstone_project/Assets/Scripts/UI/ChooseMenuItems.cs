d3624ed67d0d196dd758db5991652add80fefa9b
Sooyeon Yang <soois1114@gmail.com>
2025-09-09 16:58:31 -0500

    Prefab fixes; Finished rough setup of Choose Today's Dishes menu; Added transition from world_map to restaurant

--- team2_capstone_project/Assets/Scripts/UI/ChooseMenuItems.cs
+++ team2_capstone_project/Assets/Scripts/UI/ChooseMenuItems.cs
@@ -7,41 +7,82 @@ public class ChooseMenuItems : MonoBehaviour
 {
   public GameObject menuBox;
   public GameObject darkOverlay;
-  private bool isPaused = false;
+  private bool selectedDishes;
+  private List<string> dishesSelected = new List<string>();
+  private GameObject errorText;
 
-  // Update is called once per frame
-  void Update()
+  public void Start()
   {
-      if(Input.GetKeyDown(KeyCode.Escape))
-      {
-          if(isPaused)
-            ResumeGame();
-          else
-            PauseGame();
-      }
-  }
-
-  public void PauseGame()
-  {
-    Debug.Log("Pausing game...");
+    // Initially show the menu
     menuBox.SetActive(true);
     darkOverlay.SetActive(true);
-    isPaused = true;
+    selectedDishes = false;
+    errorText = menuBox.transform.Find("Error_Text").gameObject;
+    errorText.SetActive(false);
   }
 
   // Resume the game from the pause menu
-  public void ResumeGame()
+  public void ContinueGame()
   {
-    Debug.Log("Resuming game...");
-    menuBox.SetActive(false);
-    darkOverlay.SetActive(false);
-    isPaused = false;
+    if (dishesSelected.Count > 0)
+    {
+      Debug.Log("Continuing to resource gathering...");
+      menuBox.SetActive(false);
+      darkOverlay.SetActive(false);
+      selectedDishes = true;
+    }
+    else
+    {
+      Debug.Log("Please select at least one dish to continue.");
+      errorText.SetActive(true);
+      StartCoroutine(HideMessageAfterDelay(2f));
+    }
   }
 
-  // Quit the game from pause menu and return to the main menu
-  public void QuitGame()
+  // Coroutine to hide the error message after a delay
+  private IEnumerator HideMessageAfterDelay(float delay)
   {
-    Debug.Log("Quitting game...");
-    SceneManager.LoadScene("Main_Menu");
+    yield return new WaitForSeconds(delay);
+    errorText.SetActive(false);
+  }
+
+  // Add a dish to the selected list
+  public void addDish(string dishName)
+  {
+    if (!dishesSelected.Contains(dishName))
+    {
+      dishesSelected.Add(dishName);
+      Debug.Log(dishName + " added. Total dishes: " + dishesSelected.Count);
+    }
+    else
+    {
+      Debug.Log(dishName + " is already selected.");
+    }
+  }
+
+  // Remove a dish from the selected list
+  public void removeDish(string dishName)
+  {
+    if (dishesSelected.Contains(dishName))
+    {
+      dishesSelected.Remove(dishName);
+      Debug.Log(dishName + " removed. Total dishes: " + dishesSelected.Count);
+    }
+    else
+    {
+      Debug.Log(dishName + " is not in the selected list.");
+    }
+  }
+
+  // Get the list of selected dishes
+  public List<string> getSelectedDishes()
+  {
+    return dishesSelected;
+  }
+
+  // Check if any dishes have been selected
+  public bool hasSelectedDishes()
+  {
+    return selectedDishes;
   }
 }
--- team2_capstone_project/Assets/Scripts/UI/DishItemToggle.cs
+++ team2_capstone_project/Assets/Scripts/UI/DishItemToggle.cs
@@ -3,18 +3,52 @@ using UnityEngine.UI;
 
 public class DishItemToggle : MonoBehaviour
 {
-    public Toggle toggle;
-    public Outline outline;
+  public Toggle toggle;
 
-    void Start()
-    {
-        toggle.onValueChanged.AddListener(OnToggleChanged);
-        outline.enabled = toggle.isOn;
-    }
+  // Toggle colors
+  public Color onColor = Color.red;
+  public Color offColor = Color.white;
 
-    private void OnToggleChanged(bool isOn)
+  public Graphic targetGraphic;
+
+  void Start()
+  {
+    toggle.onValueChanged.AddListener(OnToggleChanged);
+  }
+
+  private void OnToggleChanged(bool isOn)
+  {
+    Debug.Log("toggle changed to: " + isOn);
+    UpdateColor(isOn);
+    addOrRemoveDish(isOn);
+  }
+
+  // Update the color based on the toggle state
+  private void UpdateColor(bool isOn)
+  {
+    if (targetGraphic != null)
+      targetGraphic.color = isOn ? onColor : offColor;
+  }
+
+  // Add or remove dish from the selected list in ChooseMenuItems
+  private void addOrRemoveDish(bool isOn)
+  {
+    ChooseMenuItems menu = FindObjectOfType<ChooseMenuItems>();
+    if (menu != null)
     {
-        Debug.Log("toggle changed to: " + isOn);
-        outline.enabled = toggle.isOn;
+      string dishName = gameObject.name; // Assuming the GameObject's name is the dish name
+      if (isOn)
+      {
+        menu.addDish(dishName);
+      }
+      else
+      {
+        menu.removeDish(dishName);
+      }
     }
+    else
+    {
+      Debug.LogWarning("ChooseMenuItems script not found in the scene.");
+    }
+  }
 }
