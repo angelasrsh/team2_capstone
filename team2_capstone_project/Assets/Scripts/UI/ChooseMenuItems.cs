34bce08ae7cedbe3515466d780eab7f48bc16fdc
Sooyeon Yang <soois1114@gmail.com>
2025-09-08 19:46:15 -0500

    Choose food menu WIP

---
+++ team2_capstone_project/Assets/Scripts/UI/ChooseMenuItems.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChooseMenuItems : MonoBehaviour
+{
+  public GameObject menuBox;
+  public GameObject darkOverlay;
+  private bool isPaused = false;
+
+  // Update is called once per frame
+  void Update()
+  {
+      if(Input.GetKeyDown(KeyCode.Escape))
+      {
+          if(isPaused)
+            ResumeGame();
+          else
+            PauseGame();
+      }
+  }
+
+  public void PauseGame()
+  {
+    Debug.Log("Pausing game...");
+    menuBox.SetActive(true);
+    darkOverlay.SetActive(true);
+    isPaused = true;
+  }
+
+  // Resume the game from the pause menu
+  public void ResumeGame()
+  {
+    Debug.Log("Resuming game...");
+    menuBox.SetActive(false);
+    darkOverlay.SetActive(false);
+    isPaused = false;
+  }
+
+  // Quit the game from pause menu and return to the main menu
+  public void QuitGame()
+  {
+    Debug.Log("Quitting game...");
+    SceneManager.LoadScene("Main_Menu");
+  }
+}
