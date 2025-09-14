c2da9cfd06df98de2993a59704a57412ef334d6b
KnockbackNemo <74027612+KnockbackNemo@users.noreply.github.com>
2025-09-14 12:43:57 -0500

    Made derived dish and ingredient classes
    Inventory manager objects for dish and ingredient inventory now exist.

---
+++ team2_capstone_project/Assets/Scripts/Inventory/Dish_Inventory.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// You can currently store dishes indefinitely. Probably not desired behavior,
+/// but I'm leaving it for now. We probably change it semi-easily later.
+/// </summary>
+public class Dish_Inventory : Inventory
+{
+    public static Dish_Inventory Instance { get; private set; }
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+            Destroy(gameObject);
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
