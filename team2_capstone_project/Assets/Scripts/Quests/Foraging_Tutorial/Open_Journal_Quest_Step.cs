a4c0d6721fd292b9cd22d876f3f81ee3136e595e
KnockbackNemo <74027612+KnockbackNemo@users.noreply.github.com>
2025-10-14 02:52:27 -0500

    Adding sprite textures

--- team2_capstone_project/Assets/Scripts/Quests/Foraging_Tutorial/Open_Journal_Quest_Step.cs
+++ team2_capstone_project/Assets/Scripts/Quests/Foraging_Tutorial/Open_Journal_Quest_Step.cs
@@ -2,19 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Open_Journal_Quest_Step : Tutorial_Quest_Step
+public class Open_Journal_Quest_Step : Dialogue_Quest_Step
 {
+    [SerializeField] private string textKey;
 
     void OnEnable()
     {
         Game_Events_Manager.Instance.onJournalToggle += JournalToggled;
-        DelayedInstructionStart();
+       
     }
 
     // Unsubscribe to clean up
     void OnDisable()
     {
         Game_Events_Manager.Instance.onJournalToggle -= JournalToggled;
+    }
+
+    private void Start()
+    {
+
+        // If PC, choose key that refers to the keybind instead of the icon
+        if (SystemInfo.deviceType != DeviceType.Handheld && !simulateMobile)
+        {
+            DelayedDialogue("Tutorial.Journal_PC", 3);
+        } else
+        {
+            DelayedDialogue("Tutorial.Journal_Mobile", 3);
+        }
     }
 
 
