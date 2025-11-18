6a42021ce333aa1c0aa9e8a02f6ada9f1e0a110f
KnockbackNemo <74027612+KnockbackNemo@users.noreply.github.com>
2025-11-18 02:19:26 -0600

    Spawn Asper the first time you go to the care
    Also you always go to the restaurant, but customers only spawn in the evening. I like that the flow makes me feel like I'm actually walking in the space, but we can change this if we don't like the added transitions.

--- team2_capstone_project/Assets/Scripts/Quests/Cafe_Tutorial/Get_Customer_Order_Quest_Step.cs
+++ team2_capstone_project/Assets/Scripts/Quests/Cafe_Tutorial/Get_Customer_Order_Quest_Step.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Get_Customer_Order_Quest_Step : Dialogue_Quest_Step
 {
+
+    private Customer_Spawner customerSpawner;
+    public CustomerData customerToSpawn;
+    public string key = "Asper.Get_Order_Asper";
     protected override void OnEnable()
     {
         Game_Events_Manager.Instance.onGetOrder += GetCustomerOrder;
@@ -16,9 +20,20 @@ public class Get_Customer_Order_Quest_Step : Dialogue_Quest_Step
 
     private void Start()
     {
-        DelayedDialogue(1, 0);
+        Customer_Spawner customerSpawner = GameObject.FindObjectOfType<Customer_Spawner>();
+        if (customerSpawner == null) 
+            Helpers.printLabeled(this, "No Customer_Spawner found in scene");
+        else
+        {
+            customerSpawner.SpawnSingleCustomer(customerToSpawn);
+        }
+
+
+        DelayedDialogue(1, 0, false, key);
         DelayedDialogue(10, 0, false, "Journal.Get_Order2"); // Second reminder if they still haven't gotten an order
     }
 
     private void GetCustomerOrder() => FinishQuestStep(); // Finish and destroy this object
 }
+
+// disable and enable restaurant spawning?
